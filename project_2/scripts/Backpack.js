d6c4949e999d89f2188cb87e4aac225392f96817
yourwpmadesimple <wayne@yourwpmadesimple.com>
2021-11-30 08:40:38 -0700

    initial commit for project_2

---
+++ project_2/scripts/Backpack.js
@@ -0,0 +1,43 @@
+const Backpack = class {
+  constructor(
+    name,
+    volume,
+    color,
+    pocketNum,
+    strapLengthLeft,
+    strapLengthRignt,
+    lidOpen,
+    dateAquired,
+    image,
+  ) {
+    //  Define Properties
+    this.name = name;
+    this.volume = volume;
+    this.color = color;
+    this.pocketNum = pocketNum;
+    this.strapLength = {
+      left: strapLengthLeft,
+      right: strapLengthRignt,
+    };
+    this.lidOpen = lidOpen;
+    this.dateAquired = new Date(dateAquired);
+    this.dateAquired.toDateString();
+    this.image = image;
+  }
+  // Add methods like normal functions
+  toggleLid(lidStatus) {
+    this.lidOpen = lidStatus;
+  }
+  newStrapLenth(lengthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+  }
+  backpackAge() {
+    let now = new Date();
+    let aquired = new Date(this.dateAquired);
+    let elapsed = now - aquired;
+    let daysSinceAquired = Math.floor(elapsed / (1000 * 3600 * 24));
+    return daysSinceAquired;
+  }
+};
+export default Backpack;
